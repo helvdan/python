a21aa56f7f218572b697dea057545c8956a448a0
Leonid Makeev <lmakeev@rbc.ru>
2017-05-07 22:43:46 +0300

    numbers

---
+++ mark_lutz/learning_python/chapter4/numbers.py
@@ -0,0 +1,11 @@
+# integer addition
+print 123 + 222
+
+# floating-point multiplication
+print 1.5 * 4
+
+# 2 to the power 100
+print 2 ** 100
+
+# how many digits in a really BIG number?
+print len(str(2 ** 1000000))
